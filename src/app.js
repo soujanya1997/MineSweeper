e413c28806e9324059c04be23aef0700a4609c2b
Soujanya <creatingaworldofimagination@gmail.com>
2020-05-27 00:03:13 +0530

    First Vue file

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import Header from './Header.vue'
+// import Footer from './Footer.vue'
+// import Game from './Game.vue'
+// import Minefield from './Minefield.vue'
+// import ButtonSwitch from "./ButtonSwitch.vue"
+// import MinefieldCell from "./MinefieldCell.vue"
+
+// Vue.component('minesweeper-header', Header)
+// Vue.component('minesweeper-game', Game)
+// Vue.component('minesweeper-field', Minefield)
+// Vue.component('minesweeper-field-cell', MinefieldCell)
+// Vue.component('minesweeper-footer', Footer)
+// Vue.component('app-button-switch', ButtonSwitch)
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
