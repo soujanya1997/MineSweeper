b3d12409fd1001ea2f3c764d1a602e4518a9654a
Soujanya <creatingaworldofimagination@gmail.com>
2020-05-27 01:43:51 +0530

    Created first matrix grid in Vue

--- src/app.js
+++ src/app.js
@@ -2,15 +2,56 @@ new Vue({
     el: '#app',
     data: {
         matrix: [],
-        difficulty: 3
+        difficulty: 6
+    },
+    methods: {
+        setCount: function(i,j) {
+            let top = i-1, down = i+1, left = j-1, right = j+1;
+            let count = 0 ;
+            if(top>=0)
+            {
+                if(this.matrix[top][j] === '*')
+                    count++ ;
+            }
+            if(down<this.difficulty)
+            {
+                if(this.matrix[down][j] === '*')
+                    count++ ;
+            }
+            if(left>=0)
+            {
+                if(this.matrix[i][left] === '*')
+                    count++ ;
+            }
+            if(right<this.difficulty)
+            {
+                if(this.matrix[i][right] === '*')
+                    count++ ;
+            }
+            return count ;
+        }
     },
     mounted() {
         for (let i = 0; i < this.difficulty; i++) {
             let row = [];
             for (let j = 0; j < this.difficulty; j++) {
-                row.push(".") ;
+                let mine = Math.random();
+
+                if (mine <= 0.25)
+                    row.push("*");
+                else
+                    row.push(".");
             }
-            this.matrix.push(row) ;
+            this.matrix.push(row);
+        }
+
+        for (let i = 0; i < this.difficulty; i++) {
+            for (let j = 0; j < this.difficulty; j++) {
+                if(this.matrix[i][j] === '.' )
+                {
+                    this.matrix[i][j] = this.setCount(i,j) ;
+                }
+            }
         }
     }
 })
